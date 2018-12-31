e3f58c163fd8756e31ea5b6b7074c12dddd928b5
Jouni Linden <jouni.linden@gmail.com>
2018-12-31 16:19:58 +0200

    local startup script

--- start-local.sh
+++ start-local.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-bash minify.sh && go build -mod=vendor && sudo MONGO_URL=127.0.0.1:27017 ./newsfeedreader -env local
+bash minify.sh && go build -mod=vendor && sudo MONGO_URL=192.168.0.5:27017 ./newsfeedreader -env local
