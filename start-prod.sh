08af9af0ff4ad02228a1ecde35165f810d7df4c7
Jouni Linden <jouni.linden@gmail.com>
2024-06-30 15:21:49 +0300

    fixed not killing the process when updating

--- start-prod.sh
+++ start-prod.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-bash minify.sh && 
+bash minify.sh &&
+sleep 2 && 
 go build -mod=vendor && 
-nohup  MONGO_URL="mongodb://$MONGO_USER:$MONGO_PASSWORD@192.168.0.1:27017/news" ./newsfeedreader  > news.log 2>&1&
+killall newsfeedreader
+sleep 2 &&
+export MONGO_URL="mongodb://$MONGO_USER:$MONGO_PASSWORD@192.168.0.1:27017/news"
+nohup ./newsfeedreader > news.log 2>&1&
+sleep 2
 ps aux|grep news
